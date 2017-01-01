7519373f85c3d4f65abcc487e19936918ee666c0
Martin9700 <martin@pughspace.com>
2016-12-31 23:06:41 -0500

    Add Find-Cluster and Get-ClusterHealth
    1. Find Cluster is MVP - needs comment based help
    2. Get-ClusterHealth needs parameter and initial run to be coded.
    Comment based help too.  Non-functional

---
+++ Source/Public/Find-Cluster.ps1
@@ -0,0 +1,32 @@
+﻿Function Find-Cluster {
+    <#
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(ValueFromPipeline=$true)]
+        [string]$Name = "*",
+
+        [string[]]$Exclude,
+
+        [switch]$ExcludeDisabled
+    )
+    $Excludes = $Exclude -join "|"
+
+    #Locate the raw clusters
+    $RawClusters = Get-ADComputer -Filter { servicePrincipalName -like "*MSServerCluster/*" } -Properties servicePrincipalName | Where Name -like $Name | Select Name,DNSHostName,Enabled
+    $Count = 0
+    ForEach ($Cluster in $RawClusters)
+    {
+        If ($Exclude -and $Cluster.Name -match $Excludes)
+        {
+            Continue
+        }
+        If ($ExcludeDisabled -and $Cluster.Enabled -eq $false)
+        {
+            Continue
+        }
+        Write-Output $Cluster
+        Count ++
+    }
+    Write-Verbose "Found $Count Windows Clusters"
+}
---
+++ Source/Public/Get-ClusterHealth.ps1
@@ -0,0 +1,56 @@
+﻿Function Get-ClusterHealth {
+    <#
+    #>
+
+
+
+        #Now let's get some information on them
+    ForEach ($Cluster in $RawClusters)
+    {
+        Write-Verbose "$(Get-Date):Gathering cluster information from $($Cluster.Name)"
+        Try {
+            $ClusterObject = Get-Cluster -Name $Cluster.DNSHostName -ErrorAction Stop
+            $Nodes = $ClusterObject | Get-ClusterNode
+            $ClusterGroups = $ClusterObject | Get-ClusterGroup
+        }
+        Catch {
+            [PSCustomObject]@{
+                Cluster            = $Cluster.Name
+                Enabled            = $Cluster.Enabled
+                Nodes              = "Error Connecting"
+                ClusterGroups      = $_.Exception.Message
+                IsAG               = $null
+                AvailabilityGroups = $null
+            }
+            Continue
+        }
+
+        $IsAG = $false
+        Try {
+            $AGs = Get-AGCluster -ComputerName $Cluster.DNSHostName -ErrorAction Stop
+            $Filter = ($AGs.AvailabilityGroup) -join '|'
+            $ClusterGroups = $ClusterGroups | Where Name -notmatch $Filter
+            $IsAG = $true
+        }
+        Catch {
+            If ($_.Exception.Message -like "*login failed*")
+            {
+                Write-Warning "Unable to query, need to configure PROD\WinFailover on the SQL server"
+            }
+            Else
+            {
+                Write-Verbose "No AG on this cluster"
+            }
+            $AGs = $null
+        }
+        [PSCustomObject]@{
+            Cluster            = $Cluster.Name
+            Enabled            = $Cluster.Enabled
+            Nodes              = $Nodes | Select Name,State
+            ClusterGroups      = $ClusterGroups | Select Name,State
+            IsAG               = $IsAg
+            AvailabilityGroups = $AGs | Select @{Name="Name";Expression={ $_.AvailabilityGroup }},@{Name="State";Expression={ $_.HealthState }}
+        }
+    }
+
+}
